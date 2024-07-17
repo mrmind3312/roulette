372c5595b6d127812d0aced06891e8d6e17cfccf
Josue Mente <josuemente@hotmail.com>
2024-07-16 19:57:10 -0600

    init config of nuxt js for frontend application

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./pages/**/*.vue', './components/**/*.vue'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
