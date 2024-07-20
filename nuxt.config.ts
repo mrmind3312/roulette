ab9c2d3b7ea068a0849b770eb9e127e3ff373953
Josue Mente <josuemente@hotmail.com>
2024-07-19 21:14:18 -0600

    add some base config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", '@pinia/nuxt'],
+  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
   runtimeConfig: {
     public: {
-      apiUrl: '',
-      authToken: '',
-    }
+      apiUrl: "",
+      authToken: "",
+      days: ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sábado", "Domingo"],
+      months: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    },
   },
-})
+  routeRules: {
+    "/": { redirect: "/auth/login" },
+  },
+});
